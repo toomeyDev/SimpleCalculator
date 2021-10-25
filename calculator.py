d729527dd8e3dbfc1644e122cdb31c513bf8c347
Matthew Toomey <mtoomeydev@gmail.com>
2021-10-25 12:19:56 -0400

    refactor operation handling for greater flexibility

--- calculator.py
+++ calculator.py
@@ -14,39 +14,30 @@ def intro():
     while ans != '':
         ans = input("Press enter to continue: \n")
 
+
 def menu():
-    print("Choose an operation (addition, subtraction, multiplication, division)\n")
-    ans = None
-    valid_operations = ["addition","subtraction","multiplication","division"]
-    while ans == None: 
-        ans = input("Choice: ")
-        if ans in valid_operations:
-            # if ans is a valid operation, match to corresponding function in operations
-            operation = getattr(operations, ans)
-            n1 = int(input(f"Performing {ans}, choose first number: \n"))
-            n2 = int(input(f"Choose second number: \n"))
-            answer = operation(n1, n2)
-            print(f"Result is: {answer}")
-            exit_query = input("Perform more operations? (y/n)\n").lower()
-            if exit_query == 'y':
-                ans = None
-                sleep(1)
-                clear_screen()
-            elif exit_query == 'n':
-                sleep(1)
-                clear_screen()
-                break
-            else:
-                print("Expecting y or n, defaulting to n.")
-                sleep(1)
-                clear_screen()
-        else:
-            # if ans is not a valid operation, continue asking for input    
-            ans = None
+    operators = ['+','-','/','%','*'] # store valid operators
+    
+    # prompt for input
+    user_input = input("Enter an operation (IE: '5 + 10')\n"
+    +"any detected valid operations will be executed.\n")
+
+    # parse valid numbers and operators from input
+    operation_stream = [] # store valid numbers and operators
+    for i in range(0, len(user_input)):
+        if user_input[i].isnumeric():
+            # if an integer is detected, parse and add to stream
+            operation_stream.append((int)(user_input[i]))
+        if user_input[i] in operators:
+            operation_stream.append(user_input[i])
+    print(operation_stream) # for testing
+
+        
 
 def main():
     intro()
     menu()
+
 
 def clear_screen():
     """Clear the terminal/console of any generated text."""
